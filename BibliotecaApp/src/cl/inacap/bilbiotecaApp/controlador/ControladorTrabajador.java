d244a4bcc023b6969d07453f36267d0de19b11c0
Francisco <Francisco22corvalan@gmail.com>
2021-07-30 13:34:44 -0400

    aaaaaa
    aaaaa

---
+++ BibliotecaApp/src/cl/inacap/bilbiotecaApp/controlador/ControladorTrabajador.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.inacap.bilbiotecaApp.controlador;
+
+/**
+ *
+ * @author Corvalan
+ */
+public class ControladorTrabajador {
+    
+    
+}
