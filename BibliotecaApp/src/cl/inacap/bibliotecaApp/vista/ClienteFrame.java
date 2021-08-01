69f80fc02479fe5e9cb18ce9ebb1f027f6acf1e9
Francisco <Francisco22corvalan@gmail.com>
2021-08-01 04:48:44 -0400

    Actualizacion

--- BibliotecaApp/src/cl/inacap/bibliotecaApp/vista/ClienteFrame.java
+++ BibliotecaApp/src/cl/inacap/bibliotecaApp/vista/ClienteFrame.java
@@ -42,26 +42,24 @@ public class ClienteFrame extends javax.swing.JFrame {
         jPanel3 = new javax.swing.JPanel();
         jLabel14 = new javax.swing.JLabel();
         jLabel15 = new javax.swing.JLabel();
-        agregar_trabajador_btn = new javax.swing.JButton();
+        agregar_cliente_btn = new javax.swing.JButton();
         jLabel11 = new javax.swing.JLabel();
         jLabel16 = new javax.swing.JLabel();
-        limpiar_trabajador_btn = new javax.swing.JButton();
+        limpiar_cliente_btn = new javax.swing.JButton();
         flecha1 = new javax.swing.JLabel();
-        id_trabajador_txt = new javax.swing.JTextField();
-        rut_trabajador_txt = new javax.swing.JTextField();
-        nom_trabajador_txt = new javax.swing.JTextField();
-        apePa_trabajador_txt = new javax.swing.JTextField();
+        id_cliente_txt = new javax.swing.JTextField();
+        rut_cliente_txt = new javax.swing.JTextField();
+        nom_cliente_txt = new javax.swing.JTextField();
+        ape_pa_cliente_txt = new javax.swing.JTextField();
         jLabel18 = new javax.swing.JLabel();
-        apeMa_trabajador_txt = new javax.swing.JTextField();
+        ape_ma_cliente_txt = new javax.swing.JTextField();
         jLabel19 = new javax.swing.JLabel();
-        direccion_trabajador_txt = new javax.swing.JTextField();
-        jLabel20 = new javax.swing.JLabel();
+        direccion_cliente_txt = new javax.swing.JTextField();
         correo_txt = new javax.swing.JTextField();
         jLabel21 = new javax.swing.JLabel();
-        telefono_trabajador_txt = new javax.swing.JTextField();
+        telefono_cliente_txt = new javax.swing.JTextField();
         jLabel22 = new javax.swing.JLabel();
         jLabel23 = new javax.swing.JLabel();
-        fecha_cont_trabajador_txt = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
@@ -88,17 +86,17 @@ public class ClienteFrame extends javax.swing.JFrame {
         jLabel15.setText("Nombre Cliente");
         jPanel3.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 180, -1, -1));
 
-        agregar_trabajador_btn.setBackground(new java.awt.Color(255, 255, 255));
-        agregar_trabajador_btn.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 13)); // NOI18N
-        agregar_trabajador_btn.setForeground(new java.awt.Color(0, 0, 0));
-        agregar_trabajador_btn.setActionCommand("Agregar Cliente");
-        agregar_trabajador_btn.setLabel("Agregar Trabajador");
-        agregar_trabajador_btn.addActionListener(new java.awt.event.ActionListener() {
+        agregar_cliente_btn.setBackground(new java.awt.Color(255, 255, 255));
+        agregar_cliente_btn.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 13)); // NOI18N
+        agregar_cliente_btn.setForeground(new java.awt.Color(0, 0, 0));
+        agregar_cliente_btn.setText("Agregar Cliente");
+        agregar_cliente_btn.setActionCommand("Agregar Cliente");
+        agregar_cliente_btn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                agregar_trabajador_btnActionPerformed(evt);
+                agregar_cliente_btnActionPerformed(evt);
             }
         });
-        jPanel3.add(agregar_trabajador_btn, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 530, 180, 52));
+        jPanel3.add(agregar_cliente_btn, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 480, 180, 52));
 
         jLabel11.setBackground(new java.awt.Color(11, 0, 32));
         jLabel11.setFont(new java.awt.Font("Microsoft JhengHei UI", 1, 24)); // NOI18N
@@ -112,16 +110,16 @@ public class ClienteFrame extends javax.swing.JFrame {
         jLabel16.setText("ID");
         jPanel3.add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 100, -1, -1));
 
-        limpiar_trabajador_btn.setBackground(new java.awt.Color(255, 255, 255));
-        limpiar_trabajador_btn.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 13)); // NOI18N
-        limpiar_trabajador_btn.setForeground(new java.awt.Color(0, 0, 0));
-        limpiar_trabajador_btn.setText("Limpiar texto");
-        limpiar_trabajador_btn.addActionListener(new java.awt.event.ActionListener() {
+        limpiar_cliente_btn.setBackground(new java.awt.Color(255, 255, 255));
+        limpiar_cliente_btn.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 13)); // NOI18N
+        limpiar_cliente_btn.setForeground(new java.awt.Color(0, 0, 0));
+        limpiar_cliente_btn.setText("Limpiar texto");
+        limpiar_cliente_btn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                limpiar_trabajador_btnActionPerformed(evt);
+                limpiar_cliente_btnActionPerformed(evt);
             }
         });
-        jPanel3.add(limpiar_trabajador_btn, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 530, 143, 52));
+        jPanel3.add(limpiar_cliente_btn, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 480, 143, 52));
 
         flecha1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/flecha_img.png"))); // NOI18N
         flecha1.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -131,20 +129,20 @@ public class ClienteFrame extends javax.swing.JFrame {
         });
         jPanel3.add(flecha1, new org.netbeans.lib.awtextra.AbsoluteConstraints(15, 11, 73, -1));
 
-        id_trabajador_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
-        id_trabajador_txt.setForeground(new java.awt.Color(0, 0, 0));
-        jPanel3.add(id_trabajador_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 130, 60, 35));
+        id_cliente_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
+        id_cliente_txt.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel3.add(id_cliente_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 130, 60, 35));
 
-        rut_trabajador_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
-        jPanel3.add(rut_trabajador_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 130, 280, 35));
+        rut_cliente_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
+        jPanel3.add(rut_cliente_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 130, 280, 35));
 
-        nom_trabajador_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
-        nom_trabajador_txt.setForeground(new java.awt.Color(0, 0, 0));
-        jPanel3.add(nom_trabajador_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 210, 174, 35));
+        nom_cliente_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
+        nom_cliente_txt.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel3.add(nom_cliente_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 210, 174, 35));
 
-        apePa_trabajador_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
-        apePa_trabajador_txt.setForeground(new java.awt.Color(0, 0, 0));
-        jPanel3.add(apePa_trabajador_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 290, 170, 35));
+        ape_pa_cliente_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
+        ape_pa_cliente_txt.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel3.add(ape_pa_cliente_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 290, 170, 35));
 
         jLabel18.setBackground(new java.awt.Color(102, 102, 102));
         jLabel18.setFont(new java.awt.Font("Microsoft JhengHei UI", 1, 18)); // NOI18N
@@ -152,9 +150,9 @@ public class ClienteFrame extends javax.swing.JFrame {
         jLabel18.setText("Apellido Paterno");
         jPanel3.add(jLabel18, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 260, 171, -1));
 
-        apeMa_trabajador_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
-        apeMa_trabajador_txt.setForeground(new java.awt.Color(0, 0, 0));
-        jPanel3.add(apeMa_trabajador_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 290, 170, 35));
+        ape_ma_cliente_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
+        ape_ma_cliente_txt.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel3.add(ape_ma_cliente_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 290, 170, 35));
 
         jLabel19.setBackground(new java.awt.Color(102, 102, 102));
         jLabel19.setFont(new java.awt.Font("Microsoft JhengHei UI", 1, 18)); // NOI18N
@@ -162,15 +160,9 @@ public class ClienteFrame extends javax.swing.JFrame {
         jLabel19.setText("Apellido Materno");
         jPanel3.add(jLabel19, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 260, 187, -1));
 
-        direccion_trabajador_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
-        direccion_trabajador_txt.setForeground(new java.awt.Color(0, 0, 0));
-        jPanel3.add(direccion_trabajador_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 350, 260, 35));
-
-        jLabel20.setBackground(new java.awt.Color(102, 102, 102));
-        jLabel20.setFont(new java.awt.Font("Microsoft JhengHei UI", 1, 18)); // NOI18N
-        jLabel20.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel20.setText("Fecha Contrato");
-        jPanel3.add(jLabel20, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 470, 140, 40));
+        direccion_cliente_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
+        direccion_cliente_txt.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel3.add(direccion_cliente_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 350, 260, 35));
 
         correo_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
         correo_txt.setForeground(new java.awt.Color(0, 0, 0));
@@ -182,9 +174,9 @@ public class ClienteFrame extends javax.swing.JFrame {
         jLabel21.setText("Correo");
         jPanel3.add(jLabel21, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 410, 90, 40));
 
-        telefono_trabajador_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
-        telefono_trabajador_txt.setForeground(new java.awt.Color(0, 0, 0));
-        jPanel3.add(telefono_trabajador_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 210, 170, 35));
+        telefono_cliente_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
+        telefono_cliente_txt.setForeground(new java.awt.Color(0, 0, 0));
+        jPanel3.add(telefono_cliente_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 210, 170, 35));
 
         jLabel22.setBackground(new java.awt.Color(102, 102, 102));
         jLabel22.setFont(new java.awt.Font("Microsoft JhengHei UI", 1, 18)); // NOI18N
@@ -197,10 +189,6 @@ public class ClienteFrame extends javax.swing.JFrame {
         jLabel23.setForeground(new java.awt.Color(255, 255, 255));
         jLabel23.setText("Direccion");
         jPanel3.add(jLabel23, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 350, 90, 40));
-
-        fecha_cont_trabajador_txt.setBackground(java.awt.SystemColor.controlLtHighlight);
-        fecha_cont_trabajador_txt.setForeground(new java.awt.Color(0, 0, 0));
-        jPanel3.add(fecha_cont_trabajador_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 470, 220, 35));
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/mas_img.png"))); // NOI18N
         jPanel3.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 410, 40, -1));
@@ -227,13 +215,13 @@ public class ClienteFrame extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void agregar_trabajador_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_agregar_trabajador_btnActionPerformed
+    private void agregar_cliente_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_agregar_cliente_btnActionPerformed
 
-    }//GEN-LAST:event_agregar_trabajador_btnActionPerformed
+    }//GEN-LAST:event_agregar_cliente_btnActionPerformed
 
-    private void limpiar_trabajador_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_limpiar_trabajador_btnActionPerformed
+    private void limpiar_cliente_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_limpiar_cliente_btnActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_limpiar_trabajador_btnActionPerformed
+    }//GEN-LAST:event_limpiar_cliente_btnActionPerformed
 
     private void flecha1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_flecha1MouseClicked
         GestionDatosFrame gtFrame = new GestionDatosFrame();
@@ -277,14 +265,13 @@ public class ClienteFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    public javax.swing.JButton agregar_trabajador_btn;
-    public javax.swing.JTextField apeMa_trabajador_txt;
-    public javax.swing.JTextField apePa_trabajador_txt;
+    public javax.swing.JButton agregar_cliente_btn;
+    public javax.swing.JTextField ape_ma_cliente_txt;
+    public javax.swing.JTextField ape_pa_cliente_txt;
     public javax.swing.JTextField correo_txt;
-    public javax.swing.JTextField direccion_trabajador_txt;
-    public javax.swing.JTextField fecha_cont_trabajador_txt;
+    public javax.swing.JTextField direccion_cliente_txt;
     public javax.swing.JLabel flecha1;
-    public javax.swing.JTextField id_trabajador_txt;
+    public javax.swing.JTextField id_cliente_txt;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel14;
     private javax.swing.JLabel jLabel15;
@@ -292,7 +279,6 @@ public class ClienteFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel18;
     private javax.swing.JLabel jLabel19;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel20;
     private javax.swing.JLabel jLabel21;
     private javax.swing.JLabel jLabel22;
     private javax.swing.JLabel jLabel23;
@@ -300,9 +286,9 @@ public class ClienteFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel3;
-    public javax.swing.JButton limpiar_trabajador_btn;
-    public javax.swing.JTextField nom_trabajador_txt;
-    public javax.swing.JTextField rut_trabajador_txt;
-    public javax.swing.JTextField telefono_trabajador_txt;
+    public javax.swing.JButton limpiar_cliente_btn;
+    public javax.swing.JTextField nom_cliente_txt;
+    public javax.swing.JTextField rut_cliente_txt;
+    public javax.swing.JTextField telefono_cliente_txt;
     // End of variables declaration//GEN-END:variables
 }
